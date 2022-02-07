bb832e9672e1f82c90ff5059caaea472b3ccbcb5
Aydiv <55044178+Aydivv@users.noreply.github.com>
2022-02-07 23:13:11 +0000

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDTji2BIFBq1ojc4QdZPLU81GfzsIOU5i0",
+    authDomain: "clone-a50ec.firebaseapp.com",
+    projectId: "clone-a50ec",
+    storageBucket: "clone-a50ec.appspot.com",
+    messagingSenderId: "1086788723468",
+    appId: "1:1086788723468:web:057f3fb458938952451f29",
+    measurementId: "G-NDQ3HBPSNX"
+  };
