cfabd43bcb2591c21b66454de75eb2980c31e3eb
Aydiv <55044178+Aydivv@users.noreply.github.com>
2022-02-10 00:22:49 +0000

    Login Visuals completed.

--- src/Login.js
+++ src/Login.js
@@ -12,6 +12,26 @@ function Login() {
           alt="amazon logo"
         />
       </Link>
+
+      <div className="login__container">
+            <h1>Sign In</h1>
+            <form>
+                <h5>Email</h5>
+                <input type="text" />
+
+                <h5>Password</h5>
+                <input type="password" />
+
+                <button className="login__signInButton">Sign In</button>
+            </form>
+
+            <p> By signing-in you agree to the Amazon Clone conditions of use and sale. 
+                Please see our Privacy Notice,our Cookies Notice and our Interest-Based 
+                Ads Notice.
+                </p>
+
+            <button className="login__registerButton">Create your Amazon Account</button>
+      </div>
     </div>
   );
 }
